b9619b4dc36254c7842e6c75992723f663ece41a
xiaohuai1002 <1412150466@qq.com>
2017-06-12 16:13:43 +0800

    version1.0

---
+++ IDLianLianSee1.0/src/whu/iss/sric/view/OnToolsChangeListener.java
@@ -0,0 +1,8 @@
+package whu.iss.sric.view;
+
+
+public interface OnToolsChangeListener{
+	public void onRefreshChanged(int count);
+	public void onTipMFChanged(int count);
+	public void onTipXNChanged(int count);
+}
